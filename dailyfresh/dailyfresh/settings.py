f434251806c0ffafa326de9719a6ccba1221a975
hanyy <hanyy6217@163.com>
2017-06-03 19:36:04 +0800

    首页列表页

--- dailyfresh/dailyfresh/settings.py
+++ dailyfresh/dailyfresh/settings.py
@@ -1,3 +1,4 @@
+#coding:utf-8
 """
 Django settings for dailyfresh project.
 
@@ -38,6 +39,8 @@ INSTALLED_APPS = (
     'django.contrib.messages',
     'django.contrib.staticfiles',
     'df_user',
+    'df_goods',
+    'tinymce',
 )
 
 MIDDLEWARE_CLASSES = (
@@ -108,3 +111,17 @@ STATIC_URL = '/static/'
 STATICFILES_DIRS=[
     os.path.join(BASE_DIR,'static')
 ]
+
+# 图片上传的路径
+MEDIA_ROOT=os.path.join(BASE_DIR,'static')
+#福文本编辑器
+TINYMCE_DEFAULT_CONFIG = {
+    'theme': 'advanced',
+    'width': 600,
+    'height': 400,
+}
+
+
+
+
+
--- dailyfresh/df_goods/urls.py
+++ dailyfresh/df_goods/urls.py
@@ -3,6 +3,9 @@ from django.conf.urls import url
 from . import views
 urlpatterns = [
     url(r'^$', views.index),
+    url(r'^detail/$', views.detail),
+    url(r'^list(\d+)-(\d+)-(\d+)/$', views.list),
+    url(r'^index_ajax(\d+)/$', views.index_ajax),
 
 
 ]
